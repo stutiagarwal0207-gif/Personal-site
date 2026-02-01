32b002cbedaa6a4d8a9363b9adf580c542e8862e
stutiagarwal0207-gif <stutiagarwal0207@gmail.com>
2026-02-01 16:48:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const noBtn = document.getElementById("noBtn");
+const yesBtn = document.getElementById("yesBtn");
+const loveMessage = document.getElementById("loveMessage");
+
+noBtn.addEventListener("mouseenter", moveButton);
+noBtn.addEventListener("touchstart", moveButton);
+
+function moveButton() {
+  const container = document.querySelector(".container");
+  const rect = container.getBoundingClientRect();
+
+  const x = Math.random() * (rect.width - noBtn.offsetWidth);
+  const y = Math.random() * (rect.height - noBtn.offsetHeight);
+
+  noBtn.style.left = `${x}px`;
+  noBtn.style.top = `${y}px`;
+}
+
+yesBtn.addEventListener("click", () => {
+  loveMessage.classList.remove("hidden");
+});
